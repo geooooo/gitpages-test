4349cfb47d55f5ce11ac06af6b185b31265371ec
geooooo <geo97it@gmail.com>
2018-06-13 22:31:52 +0300

    init project

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var pug  = require('gulp-pug');
+var scss = require('gulp-sass');
+
+
+var PATH = {
+    'pug': {
+        'src':  'index.pug',
+        'dest': '.',
+    },
+    'scss': {
+        'src':  'css/*.scss',
+        'dest': 'css',
+    },
+};
+
+
+gulp.task('compile-pug', function () {
+    return gulp.src(PATH.pug.src)
+               .pipe(pug({
+                   'pretty': true,
+               }))
+               .pipe(gulp.dest(PATH.pug.dest));
+});
+
+
+gulp.task('compile-scss', function () {
+    return gulp.src(PATH.scss.src)
+               .pipe(scss({
+                    'outputStyle': 'expanded',
+               }))
+               .pipe(gulp.dest(PATH.scss.dest));
+});
+
+
+gulp.task('compile', function () {
+    gulp.watch(PATH.pug.src,  gulp.series('compile-pug'));
+    gulp.watch(PATH.scss.src, gulp.series('compile-scss'));
+});
